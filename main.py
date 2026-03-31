da14804f072d7d3aa162e2e16676a17da69e3bb6
Nikhil <nikhilwork45@gmail.com>
2026-03-31 23:09:03 +0530

    Creating structured output using pydantic

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+from typing import List
+
 from dotenv import load_dotenv
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
-#from tavily import TavilyClient
+# from tavily import TavilyClient
 from langchain_tavily import TavilySearch
+from pydantic import BaseModel, Field
 
 load_dotenv(override=True)
 
@@ -24,14 +27,37 @@ def search(query: str) -> str:
     return tavily.search(query=query)
 
  """
+
+
+class Source(BaseModel):
+    """Schema for a source used by the agent"""
+
+    url: str = Field(description="The URL of the source")
+
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+
+    answer: str = Field(description="The agent's answer to the query")
+    sources: List[Source] = Field(
+        default_factory=list, description="List of sources used to generate the answers"
+    )
+
+
 llm = ChatOpenAI()
-tools = [TavilySearch()] #tools = [search]
-agent = create_agent(model=llm, tools= tools)
+tools = [TavilySearch()]  # tools = [search]
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 
 def main():
     print("Hello from ai-job-hunt-agent!")
-    result = agent.invoke({"messages": HumanMessage(content="Search for 3 job posting for an AI engineer using Langchain in Bangalore on Linkedin and list their details")})
+    result = agent.invoke(
+        {
+            "messages": HumanMessage(
+                content="Search for 3 job posting for an AI engineer using Langchain in Bangalore on Linkedin and list their details"
+            )
+        }
+    )
     print(result)
 
 
