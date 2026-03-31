d3f4f54591f557100a5a4647582642c0b3505ce2
Nikhil <nikhilwork45@gmail.com>
2026-03-31 22:55:07 +0530

    Combining both tavily and langchain-tavily packages

--- main.py
+++ main.py
@@ -3,12 +3,29 @@ from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
 from langchain_openai import ChatOpenAI
+#from tavily import TavilyClient
 from langchain_tavily import TavilySearch
 
 load_dotenv(override=True)
 
+""" 
+tavily = TavilyClient()
+
+@tool
+def search(query: str) -> str:
+
+    Tool that searches over the internet
+    Args:
+        query: The query to search for
+    Returns:
+        The search Result
+
+    print(f"Searching for {query}")
+    return tavily.search(query=query)
+
+ """
 llm = ChatOpenAI()
-tools = [TavilySearch()]
+tools = [TavilySearch()] #tools = [search]
 agent = create_agent(model=llm, tools= tools)
 
 
